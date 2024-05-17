a869f10284afeba25a3c6d0b817035a20adc4999
Erick <erick_ecadena@hotmail.com>
2024-05-17 15:21:11 -0600

    Primer commit de la app y agregando el Enviroment

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(Enviroment.movieDbKey),
+      ),
+    );
+  }
+}
